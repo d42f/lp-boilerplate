1d87bb75b59172f8162b78f4a85072f80147a149
d42f <dmitri.frolof@gmail.com>
2015-08-06 16:34:53 +0300

    v0.0.1

---
+++ Gruntfile.js
@@ -0,0 +1,503 @@
+module.exports = function ( grunt ) {
+
+  /** 
+   * Load required Grunt tasks. These are installed based on the versions listed
+   * in `package.json` when you do `npm install` in this directory.
+   */
+  grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-connect-rewrite');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  /**
+   * Load in our build configuration file.
+   */
+  var userConfig = require( './build.config.js' );
+
+  /**
+   * This is the configuration object Grunt uses to give each plugin its 
+   * instructions.
+   */
+  var taskConfig = {
+    /**
+     * We read in our `package.json` file so we can access the package name and
+     * version. It's already there, so we don't repeat ourselves here.
+     */
+    pkg: grunt.file.readJSON("package.json"),
+
+    /**
+     * The banner is the comment that is placed at the top of our compiled 
+     * source files. It is first processed as a Grunt template, where the `<%=`
+     * pairs are evaluated based on this very configuration object.
+     */
+    meta: {
+      banner: 
+        '/**\n' +
+        ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        ' * <%= pkg.homepage %>\n' +
+        ' *\n' +
+        ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+        ' * Licensed <%= pkg.licenses.type %> <<%= pkg.licenses.url %>>\n' +
+        ' */\n'
+    },
+
+    /**
+     * Increments the version number, etc.
+     */
+    bump: {
+      options: {
+        files: [
+          "package.json", 
+          "bower.json"
+        ],
+        commit: false,
+        commitMessage: 'chore(release): v%VERSION%',
+        commitFiles: [
+          "package.json", 
+          "client/bower.json"
+        ],
+        createTag: false,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        pushTo: 'origin'
+      }
+    },    
+
+    /**
+     * The directories to delete when `grunt clean` is executed.
+     */
+    clean: [ 
+      '<%= build_dir %>', 
+      '<%= compile_dir %>'
+    ],
+
+    /**
+     * The `copy` task just copies files from A to B. We use it here to copy
+     * our project assets (images, fonts, etc.) and javascripts into
+     * `build_dir`, and then to copy the assets to `compile_dir`.
+     */
+    copy: {
+      build_app_assets: {
+        files: [
+          { 
+            src: [ '**' ],
+            dest: '<%= build_dir %>/assets/',
+            cwd: 'src/assets',
+            expand: true
+          }
+       ]   
+      },
+      build_vendor_assets: {
+        files: [
+          { 
+            src: [ '<%= vendor_files.assets %>' ],
+            dest: '<%= build_dir %>/assets/',
+            cwd: '.',
+            expand: true,
+            flatten: true
+          }
+       ]   
+      },
+      build_appjs: {
+        files: [
+          {
+            src: [ '<%= app_files.js %>' ],
+            dest: '<%= build_dir %>/',
+            cwd: '.',
+            expand: true
+          }
+        ]
+      },
+      build_vendorjs: {
+        files: [
+          {
+            src: [ '<%= vendor_files.js %>' ],
+            dest: '<%= build_dir %>/',
+            cwd: '.',
+            expand: true
+          }
+        ]
+      },
+      build_vendorcss: {
+        files: [
+          {
+            src: [ '<%= vendor_files.css %>' ],
+            dest: '<%= build_dir %>/',
+            cwd: '.',
+            expand: true
+          }
+        ]
+      },
+      compile_assets: {
+        files: [
+          {
+            src: [ '**' ],
+            dest: '<%= compile_dir %>/assets',
+            cwd: '<%= build_dir %>/assets',
+            expand: true
+          },
+          {
+            src: [ '<%= vendor_files.css %>' ],
+            dest: '<%= compile_dir %>/',
+            cwd: '.',
+            expand: true
+          }
+        ]
+      }
+    },
+
+    /**
+     * `grunt concat` concatenates multiple source files into a single file.
+     */
+    concat: {
+      /**
+       * The `build_css` target concatenates compiled CSS and vendor CSS
+       * together.
+       */
+      build_css: {
+        src: [
+          '<%= vendor_files.css %>',
+          '<%= build_dir %>/assets/<%= pkg.name %>.css'
+        ],
+        dest: '<%= build_dir %>/assets/<%= pkg.name %>.css'
+      },
+      /**
+       * The `compile_js` target is the concatenation of our application source
+       * code and all specified vendor source code into a single file.
+       */
+      compile_js: {
+        options: {
+          banner: '<%= meta.banner %>'
+        },
+        src: [
+          '<%= vendor_files.js %>',
+          '<%= build_dir %>/src/**/*.js'
+        ],
+        dest: '<%= compile_dir %>/assets/<%= pkg.name %>.js'
+      }
+    },
+
+    /**
+     * Minify the sources!
+     */
+    uglify: {
+      compile: {
+        options: {
+          banner: '<%= meta.banner %>'
+        },
+        files: {
+          '<%= concat.compile_js.dest %>': '<%= concat.compile_js.dest %>'
+        }
+      }
+    },
+
+    /**
+     * `grunt-contrib-less` handles our LESS compilation and uglification automatically.
+     * Only our `main.less` file is included in compilation; all other files
+     * must be imported from this file.
+     */
+    less: {
+      build: {
+        files: {
+          '<%= build_dir %>/assets/<%= pkg.name %>.css': '<%= app_files.less %>'
+        }
+      },
+      compile: {
+        files: {
+          '<%= build_dir %>/assets/<%= pkg.name %>.css': '<%= app_files.less %>'
+        },
+        options: {
+          cleancss: true,
+          compress: true
+        }
+      }
+    },
+
+    /**
+     * `jshint` defines the rules of our linter as well as which files we
+     * should check.
+     */
+    jshint: {
+      src: [ 
+        '<%= app_files.js %>'
+      ],
+      gruntfile: [
+        'Gruntfile.js'
+      ],
+      options: {
+        curly: true,
+        immed: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        boss: true,
+        eqnull: true
+      },
+      globals: {}
+    },
+
+    /**
+     * Minify the html!
+     */
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          '<%= compile_dir %>/index.html': '<%= compile_dir %>/index.html'
+        }
+      }
+    },
+
+    /**
+     * The `index` task compiles the `index.html` file as a Grunt template. CSS
+     * and JS files co-exist here but they get split apart later.
+     */
+    index: {
+
+      /**
+       * During development, we don't want to have wait for compilation,
+       * concatenation, minification, etc. So to avoid these steps, we simply
+       * add all script files directly to the `<head>` of `index.html`. The
+       * `src` property contains the list of included files.
+       */
+      build: {
+        dir: '<%= build_dir %>',
+        src: [
+          '<%= vendor_files.js %>',
+          '<%= build_dir %>/src/**/*.js',
+          '<%= vendor_files.css %>',
+          '<%= build_dir %>/assets/<%= pkg.name %>.css'
+        ]
+      },
+
+      /**
+       * When it is time to have a completely compiled application, we can
+       * alter the above to include only a single JavaScript and a single CSS
+       * file. Now we're back!
+       */
+      compile: {
+        dir: '<%= compile_dir %>',
+        src: [
+          '<%= concat.compile_js.dest %>',
+          '<%= vendor_files.css %>',
+          '<%= build_dir %>/assets/<%= pkg.name %>.css'
+        ]
+      }
+    },
+
+    /**
+     * And for rapid development, we have a watch set up that checks to see if
+     * any of the files listed below change, and then to execute the listed 
+     * tasks when they do. This just saves us from having to type "grunt" into
+     * the command-line every time we want to see what we're working on; we can
+     * instead just leave "grunt watch" running in a background terminal. Set it
+     * and forget it, as Ron Popeil used to tell us.
+     *
+     * But we don't need the same thing to happen for all the files. 
+     */
+    delta: {
+      /**
+       * By default, we want the Live Reload to work for all tasks; this is
+       * overridden in some tasks (like this file) where browser resources are
+       * unaffected. It runs by default on port 35729, which your browser
+       * plugin should auto-detect.
+       */
+      options: {
+        livereload: true
+      },
+
+      /**
+       * When the Gruntfile changes, we just want to lint it. In fact, when
+       * your Gruntfile changes, it will automatically be reloaded!
+       */
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: [ 'jshint:gruntfile' ],
+        options: {
+          livereload: false
+        }
+      },
+
+      /**
+       * When our JavaScript source files change, we want to run lint them.
+       */
+      jssrc: {
+        files: [ 
+          '<%= app_files.js %>'
+        ],
+        tasks: [ 'jshint:src', 'copy:build_appjs' ]
+      },
+
+      /**
+       * When assets are changed, copy them. Note that this will *not* copy new
+       * files, so this is probably not very useful.
+       */
+      assets: {
+        files: [ 
+          'src/assets/**/*'
+        ],
+        tasks: [ 'copy:build_app_assets', 'copy:build_vendor_assets' ]
+      },
+
+      /**
+       * When index.html changes, we need to compile it.
+       */
+      html: {
+        files: [ '<%= app_files.html %>' ],
+        tasks: [ 'index:build' ]
+      },
+
+      /**
+       * When the CSS files change, we need to compile and minify them.
+       */
+      less: {
+        files: [ 'src/**/*.less' ],
+        tasks: [ 'less:build' ]
+      }
+    },
+    connect: {
+      options: {
+        port: 3000,
+        keepalive: true,
+        middleware: function (connect, options) {
+          var middlewares = [];
+
+          // Include the rewriteRequest snippet
+          middlewares.push(require('grunt-connect-rewrite/lib/utils').rewriteRequest);
+
+          if (!Array.isArray(options.base)) {
+            options.base = [options.base];
+          }
+
+          var directory = options.directory || options.base[options.base.length - 1];
+          options.base.forEach(function (base) {
+            middlewares.push(connect.static(base));
+          });
+
+          middlewares.push(connect.directory(directory));
+
+          return middlewares;
+        }
+      },
+      server: {
+        options: {
+          base: 'build'
+        }
+      },
+      server_bin: {
+        options: {
+          base: 'bin'
+        }
+      },
+      rules: [
+        {from: '^/(assets|src|vendor)/(.*)$', to: '/$1/$2'},
+        {from: '^.+$', to: '/index.html'}
+      ]
+    }
+  };
+
+  grunt.initConfig( grunt.util._.extend( taskConfig, userConfig ) );
+
+  /**
+   * In order to make it safe to just compile or copy *only* what was changed,
+   * we need to ensure we are starting from a clean, fresh build. So we rename
+   * the `watch` task to `delta` (that's why the configuration var above is
+   * `delta`) and then add a new task called `watch` that does a clean build
+   * before watching for changes.
+   */
+  grunt.renameTask( 'watch', 'delta' );
+  grunt.registerTask( 'watch', [ 'build', 'delta' ] );
+
+  /**
+   * The default task is to build and compile.
+   */
+  grunt.registerTask( 'default', [ 'build', 'compile' ] );
+
+  /**
+   * The `build` task gets your app ready to run for development.
+   */
+  grunt.registerTask( 'build', [
+    'clean', 'jshint', 'less:build',
+    'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
+    'copy:build_appjs', 'copy:build_vendorjs', 'copy:build_vendorcss', 'index:build'
+  ]);
+
+  /**
+   * The `compile` task gets your app ready for deployment by concatenating and
+   * minifying your code.
+   */
+  grunt.registerTask( 'compile', [
+    'less:compile', 'copy:compile_assets', 'concat:compile_js', 'uglify', 'index:compile', 'htmlmin'
+  ]);
+
+    grunt.registerTask( 'serv', function ( target ) {
+    grunt.task.run([
+      'configureRewriteRules', 'connect:server'
+    ]);
+  });
+
+  grunt.registerTask( 'serv_bin', function ( target ) {
+    grunt.task.run([
+      'configureRewriteRules', 'connect:server_bin'
+    ]);
+  });
+
+  /** 
+   * The index.html template includes the stylesheet and javascript sources
+   * based on dynamic names calculated in this Gruntfile. This task assembles
+   * the list into variables for the template to use and then runs the
+   * compilation.
+   */
+  grunt.registerMultiTask( 'index', 'Process index.html template', function () {
+    /**
+     * A utility function to get all app JavaScript sources.
+     */
+    function filterForJS ( files ) {
+      return files.filter( function ( file ) {
+        return file.match( /\.js$/ );
+      });
+    }
+
+    /**
+     * A utility function to get all app CSS sources.
+     */
+    function filterForCSS ( files ) {
+      return files.filter( function ( file ) {
+        return file.match( /\.css$/ );
+      });
+    }
+
+    var dirRE = new RegExp( '^(' + grunt.config('build_dir') + '|' + grunt.config('compile_dir') + ')\/', 'g' );
+    var jsFiles = filterForJS( this.filesSrc ).map( function ( file ) {
+      return file.replace( dirRE, '' );
+    });
+    var cssFiles = filterForCSS( this.filesSrc ).map( function ( file ) {
+      return file.replace( dirRE, '' );
+    });
+
+    grunt.file.copy('src/index.html', this.data.dir + '/index.html', { 
+      process: function ( contents, path ) {
+        return grunt.template.process( contents, {
+          data: {
+            scripts: jsFiles,
+            styles: cssFiles,
+            timestamp: userConfig.timestamp,
+            version: grunt.config( 'pkg.version' )
+          }
+        });
+      }
+    });
+  });
+
+};
---
+++ src/app/app.js
@@ -0,0 +1,5 @@
+;(function ($) {
+  $(function () {
+    console.log('DOM ready');
+  });
+})(jQuery);
